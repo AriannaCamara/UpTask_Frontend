718c8c922beaa7670828e5311bc42413c247d2a3
Arianna Camara <ariannaisabel795@gmail.com>
2025-12-07 07:17:39 +0100

    first commit

---
+++ src/locales/es.ts
@@ -0,0 +1,7 @@
+export const statusTranslations : { [key: string] : string }= {
+    pending: 'Pendiente',
+    onhold: 'En Espera',
+    inProgress: 'En Progreso',
+    underReview: 'En Revisión',
+    completed: 'Completado',
+}
